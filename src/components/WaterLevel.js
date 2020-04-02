784fc80691605e47d76ac1c6e9e461b8db4092a6
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-04-02 13:43:55 -0400

    choosing a range of water level values allows for more flexibility in adding precise features and lines.

--- src/components/WaterLevel.js
+++ src/components/WaterLevel.js
@@ -2,22 +2,25 @@ import React from "react";
 import "./WaterLevel.css";
 
 const WaterLevel = props => {
-
   const handleClick = e => {
-    props.selectLevel(e.currentTarget.id);
+    let x = Number(e.currentTarget.id);
+    let newLevel = [x, x + increment];
+    props.selectLevel(newLevel);
   };
 
-  const increment = 6 //Change this to change what guage increments
-  const range = [0, 20] //Indicates minimum and maximum yearly river levels
+  const increment = 6; //Change this to change what guage increments
+  const range = [0, 20]; //Indicates minimum and maximum yearly river levels
 
-  let levelMarker = []
+  let levelMarker = [];
 
-  for (let i=range[0]; i<range[1]; i += increment){
+  for (let i = range[0]; i < range[1]; i += increment) {
     levelMarker.push(
-      <div id={[i,i+increment]} className="level" onClick={handleClick}>  
-          <div className="desc">{i}ft to {i+increment}ft</div>
+      <div id={i} className="level" onClick={handleClick} key={i}>
+        <div className="desc">
+          {i}ft to {i + increment}ft
         </div>
-    )
+      </div>
+    );
   }
 
   return (
@@ -25,11 +28,7 @@ const WaterLevel = props => {
       <div id="title"> Water Level</div>
       <div id="subtitle"> Owl Gauge</div>
 
-      <div id="level-container">
-        
-      {levelMarker}
-        
-      </div>
+      <div id="level-container">{levelMarker}</div>
     </div>
   );
 };
