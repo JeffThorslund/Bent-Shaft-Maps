cc776a67c44be6729fd4ba5bd2e2e89a11a43fba
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-04-03 17:54:39 -0400

    a test environment to understand how to manipulate svgs

--- src/components/Line.js
+++ src/components/Line.js
@@ -3,8 +3,20 @@ import "./Line.css";
 
 const Line = props => {
   return (
-    <div>
-      {/* <svg
+    <div className="Line">
+      <svg
+        height='100px'
+        width='100px'
+        xmlns="http://www.w3.org/2000/svg"
+      >
+    
+
+        <circle cx="50" cy="50" r="50" fill="green" />
+
+      
+      </svg>
+
+       {/*<svg
       fill="none"
       viewBox="0 0 153 50"
       preserveAspectRatio='none'
