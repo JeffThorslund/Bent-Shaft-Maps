97e3ea2d14b099907f9b989d015babab790ce8b2
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-04-08 21:09:37 -0400

    Rapid.js arrays now clearer with less code

--- src/components/Rapid.js
+++ src/components/Rapid.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
 import './Rapid.css';
 import Hydraulic from './Hydraulic';
 import Line from './Line';
 import Display from './Display';
 import NextRapid from './NextRapid';
-
 
 class Rapid extends Component {
   constructor(props) {
@@ -34,70 +32,55 @@ class Rapid extends Component {
 
   render() {
     // render array of hydraulics based on selected water level (App state)
-    const hydraulicArray = [];
-
-    for (let i = 0; i < this.props.data.hydraulics.length; i += 1) {
+    const hydraulicArray = this.props.data.hydraulics.map((element, key) => {
       if (
-        this.props.level <= this.props.data.hydraulics[i].range[1]
-        && this.props.level >= this.props.data.hydraulics[i].range[0]
+        this.props.level <= element.range[1]
+        && this.props.level >= element.range[0]
       ) {
-        hydraulicArray.push(
-          <Hydraulic
-            level={this.props.level}
-            desc={this.props.data.hydraulics[i].desc}
-            name={this.props.data.hydraulics[i].name}
-            top={this.props.data.hydraulics[i].top}
-            left={this.props.data.hydraulics[i].left}
-            height={this.props.data.hydraulics[i].height}
-            width={this.props.data.hydraulics[i].width}
-            rotation={this.props.data.hydraulics[i].rotation}
-            displayData={this.displayData}
-            key={i}
-          />,
-        );
+        return <Hydraulic
+          level={this.props.level}
+          desc={element.desc}
+          name={element.name}
+          top={element.top}
+          left={element.left}
+          height={element.height}
+          width={element.width}
+          rotation={element.rotation}
+          displayData={this.displayData}
+          key={`hydraulic${key}`}
+        />;
       }
-    }
+      return null;
+    });
 
     // render array of lines based on selected water level (App state)
-    const lineArray = [];
-
-    for (let i = 0; i < this.props.data.lines.length; i += 1) {
+    const lineArray = this.props.data.lines.map((element, key) => {
       if (
-        this.props.level <= this.props.data.lines[i].range[1]
-        && this.props.level >= this.props.data.lines[i].range[0]
+        this.props.level <= element.range[1]
+        && this.props.level >= element.range[0]
       ) {
-        lineArray.push(<Line vector={this.props.data.lines[i].vector} key={i} />);
+        return <Line vector={element.vector} key={`line${key}`} />;
       }
-    }
+      return null;
+    });
 
     // render array of "next rapid arrows" based on selected water level (App state)
-    const arrowArray = [];
-
-    for (let i = 0; i < this.props.data.arrows.length; i += 1) {
-      arrowArray.push(
-        <NextRapid
-          rotation={this.props.data.arrows[i].rotation}
-          bottom={this.props.data.arrows[i].bottom}
-          right={this.props.data.arrows[i].right}
-          name={this.props.data.arrows[i].name}
-          selectRapid={this.props.selectRapid}
-          key={i}
-        />,
-      );
-    }
+    const arrowArray = this.props.data.arrows.map((element, key) => <NextRapid
+      rotation={element.rotation}
+      bottom={element.bottom}
+      right={element.right}
+      name={element.name}
+      selectRapid={this.props.selectRapid}
+      key={`arrow${key}`}
+    />);
 
     return (
-      <div className="Rapid">
+      <div className="Rapid" >
         <div id="rapid-name"> {this.props.data.name} </div>
-
         <div id="line-array"> {lineArray} </div>
-
         <div id="hydraulic-array"> {hydraulicArray} </div>
-
         <div id="arrow-array"> {arrowArray} </div>
-
         <Display title={this.state.title} desc={this.state.desc} />
-
         <img src={this.props.data.map} alt={this.props.data.name} id="background" />
       </div>
     );
