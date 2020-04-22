27bae6cd42a15995a115fc0aeffdc069bf20dcd3
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-04-22 17:50:36 -0400

    Search functionality done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import GlobalRouter from "./components/GlobalRouter";
+import GlobalRouter from "./components/Interface/GlobalRouter";
 
 const App = () => {
   console.log("App is rendered");
