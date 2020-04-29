d95eaa248e2f1469c90b5f5ed6568445be40d1f3
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-04-29 16:52:37 -0400

    made template more explanatory

--- src/tools/CLI/addRapid/appendDataFile.js
+++ src/tools/CLI/addRapid/appendDataFile.js
@@ -98,36 +98,4 @@ module.exports = function (answers) {
     if (err) throw err;
     console.log("appendDataFil Complete.");
   });
-
-  /*
-  //Fill import info based on user input
-  var importBlob = `import {
-  data as ${nameParser.data(answers.riverName)},
-  global as ${nameParser.global(answers.riverName)},
-} from "../river-data/${nameParser.folderName(
-    answers.riverName
-  )}/${nameParser.dataFile(answers.riverName)}";`;
-
-  //Fill RiverList element based on user input
-  var entryBlob = `{
-  name: "${answers.riverName}",
-  location: "${answers.locationName}",
-  class: "II",
-  underConst: false,
-  data: ${nameParser.data(answers.riverName)},
-  global: ${nameParser.global(answers.riverName)},
-}`;
-
-
-  result = result.replace(
-    /\/\/importEntryPoint/,
-    `${importBlob} //importEntryPoint`
-  );
-
-  //Replace old RiverList with new RiverList
-  let wstream = fs.createWriteStream(`./src/river-data/RiverList.js`);
-  wstream.write(`${result}`, function (err) {
-    if (err) throw err;
-    console.log("appendRiverList Complete.");
-  });*/
 };
