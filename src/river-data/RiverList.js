24c85b37f4fab46e1ae12fdd4c56240ce7c7ce5a
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-04-30 15:37:02 -0400

    fixing the RiverList from all deleted files

--- src/river-data/RiverList.js
+++ src/river-data/RiverList.js
@@ -2,37 +2,7 @@ import {
   data as ottawaRiverData,
   global as ottawaRiverGlobal,
 } from "../river-data/ottawa-river/data.js";
- import {
-  data as appleRiverData,
-  global as appleRiverGlobal,
-} from "../river-data/apple-river/data.js"; import {
-  data as wakaWakaData,
-  global as wakaWakaGlobal,
-} from "../river-data/waka-waka/data.js"; import {
-  data as oopdieData,
-  global as oopdieGlobal,
-} from "../river-data/oopdie/data.js"; import {
-  data as orgoData,
-  global as orgoGlobal,
-} from "../river-data/orgo/data.js"; import {
-  data as upsideDownData,
-  global as upsideDownGlobal,
-} from "../river-data/upside-down/data.js"; import {
-  data as yupData,
-  global as yupGlobal,
-} from "../river-data/yup/data.js"; import {
-  data as jordanData,
-  global as jordanGlobal,
-} from "../river-data/jordan/data.js"; import {
-  data as randomHouseData,
-  global as randomHouseGlobal,
-} from "../river-data/random-house/data.js"; import {
-  data as woahCoolAmasdfn%#432Data,
-  global as woahCoolAmasdfn%#432Global,
-} from "../river-data/woah-cool-amasdfn%#432/data.js"; import {
-  data as asdfsadfData,
-  global as asdfsadfGlobal,
-} from "../river-data/asdfsadf/data.js"; //importEntryPoint
+//importEntryPoint
 
 const RiverList = [
   {
@@ -59,76 +29,6 @@ const RiverList = [
     data: null,
     global: null,
   },
-   {
-  name: "Apple River",
-  location: "undefined",
-  class: "II",
-  underConst: false,
-  data: appleRiverData,
-  global: appleRiverGlobal,
-}, {
-  name: "WakaWaka",
-  location: "undefined",
-  class: "II",
-  underConst: false,
-  data: wakaWakaData,
-  global: wakaWakaGlobal,
-}, {
-  name: "oopdie",
-  location: "undefined",
-  class: "II",
-  underConst: false,
-  data: oopdieData,
-  global: oopdieGlobal,
-}, {
-  name: "orgo",
-  location: "undefined",
-  class: "II",
-  underConst: false,
-  data: orgoData,
-  global: orgoGlobal,
-}, {
-  name: "upside down",
-  location: "undefined",
-  class: "II",
-  underConst: false,
-  data: upsideDownData,
-  global: upsideDownGlobal,
-}, {
-  name: "Yup",
-  location: "undefined",
-  class: "II",
-  underConst: false,
-  data: yupData,
-  global: yupGlobal,
-}, {
-  name: "Jordan",
-  location: "undefined",
-  class: "II",
-  underConst: false,
-  data: jordanData,
-  global: jordanGlobal,
-}, {
-  name: "RandomHouse",
-  location: "undefined",
-  class: "II",
-  underConst: false,
-  data: randomHouseData,
-  global: randomHouseGlobal,
-}, {
-  name: "Woah Cool amasdfn%#432",
-  location: "undefined",
-  class: "II",
-  underConst: false,
-  data: woahCoolAmasdfn%#432Data,
-  global: woahCoolAmasdfn%#432Global,
-}, {
-  name: "asdfsadf",
-  location: "undefined",
-  class: "II",
-  underConst: false,
-  data: asdfsadfData,
-  global: asdfsadfGlobal,
-}, //elementEntryPoint
+  //elementEntryPoint
 ];
 export default RiverList;
