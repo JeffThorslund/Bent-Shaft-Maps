aa1bd09f6131122155f93f8bd40212241854d075
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-05-04 15:55:01 -0400

    font update, generic toggle scalable, redo mccoys lines, position gauge, map labels scalable, other changes

--- src/components/Symbols/Home.js
+++ src/components/Symbols/Home.js
@@ -11,8 +11,8 @@ const Home = () => {
   return (
     <Link to="/">
       <svg
-        width="53"
-        height="51"
+        width="3.5vw"
+        height="7vh"
         viewBox="0 0 53 51"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
