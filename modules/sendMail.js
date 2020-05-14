8fc835bd298fba4b4fe12d9ccd616b341b2154cc
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-05-14 12:38:03 -0400

    reorganizing file structure to work with back end

--- modules/sendMail.js
+++ modules/sendMail.js
@@ -1,23 +1,24 @@
 "use strict";
 const nodemailer = require("nodemailer");
-require("dotenv").config();
 
-exports.handler = function (event, context, callback) {
-  const { img, desc, river, rapid } = JSON.parse(event.body);
+module.exports = function (body) {
+
+  const { img, desc, river, rapid } = body;
+  console.log(body)
   const {
     DESTINATION_EMAIL,
     SOURCE_EMAIL,
     SOURCE_HOST,
     SOURCE_PASS,
     SOURCE_PORT,
-  } = process.env; //deconstructing environment variables from Netlify.
+  } = process.env;
 
   async function main() {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
       host: SOURCE_HOST,
       port: SOURCE_PORT,
-      secure: true, // true for 465, false for other ports
+      secure: true,
       auth: {
         user: SOURCE_EMAIL,
         pass: SOURCE_PASS,
@@ -44,14 +45,4 @@ exports.handler = function (event, context, callback) {
   }
 
   main().catch(console.error);
-
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers":
-        "Origin, X-Requested-With, Content_Type, Accept",
-    },
-    body: "sendMail.js sucessfully executed",
-  });
 };
--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-const nameReader = require("./modules/nameReader");
+const readRiverDataFiles = require("./modules/readRiverDataFiles");
+const sendMail = require("./modules/sendMail");
 const compression = require("compression");
-var bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
 const app = express();
-
 app.use(compression());
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, "client/build")));
-
-app.use(bodyParser.json())
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 app.post("/api/data", (req, res, next) => {
-  res.setHeader('Content-Type', 'application/json');
-  data = nameReader(req.body.path)
+  res.setHeader("Content-Type", "application/json");
+  data = readRiverDataFiles(req.body.path);
   res.send(data);
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+app.post("/api/mailer", (req, res, next) => {
+  sendMail(req.body);
+  res.send("Submitted!");
 });
 
 const port = process.env.PORT || 5000;
