ba2713dca6d2223f23ec107cbc12a6282624d804
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-05-20 16:47:39 -0400

    added small validation script for manual validation. Probably worth it to use Formik validation

--- client/src/components/form/InputArea.js
+++ client/src/components/form/InputArea.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Formik, Form, Field, FieldArray } from "formik";
-import { capitalCase } from "change-case";
-import { paramCase } from "change-case";
+import { capitalCase, paramCase } from "change-case";
+import { validate } from "./Validation";
 const axios = require("axios");
 
 class InputArea extends React.Component {
@@ -21,12 +21,9 @@ class InputArea extends React.Component {
           )}/maps`,
         })
         .then((response) => {
-          console.log(response.data);
           this.setState({ riverList: JSON.parse(response.data).list });
         })
-        .catch((error) => {
-          console.log(error);
-        });
+        .catch((error) => {});
     }
   }
 
@@ -34,94 +31,95 @@ class InputArea extends React.Component {
     const { river, rapid, feature, featureName, dataArr } = this.props;
 
     return (
-      <div className="input-area">
-        <Formik
-          initialValues={dataArr} //set values:_________
-          onSubmit={(values) => {
-            console.log(values);
-          }}
-        >
-          {({ values, handleChange, handleSubmit, isSubmitting }) => {
-            const data =
-              feature !== null
-                ? values[river].rapids[rapid][featureName][feature]
-                : rapid !== null
-                ? values[river].rapids[rapid]
-                : river !== null
-                ? values[river]
-                : null;
+      <>
+        <div className="input-area">
+          <Formik
+            initialValues={this.props.dataArr} //set values:_________
+            onSubmit={(values) => {
+              console.log(values);
+            }}
+          >
+            {({ values }) => {
+              const data =
+                feature !== null
+                  ? values[river].rapids[rapid][featureName][feature]
+                  : rapid !== null
+                  ? values[river].rapids[rapid]
+                  : river !== null
+                  ? values[river]
+                  : null;
 
-            let name =
-              feature !== null
-                ? `[${river}][rapids][${rapid}][${featureName}][${feature}]`
-                : rapid !== null
-                ? `[${river}][rapids][${rapid}]`
-                : river !== null
-                ? `[${river}]`
-                : null;
-            let list = [];
-            const parseObject = (dataObj, name) => {
-              for (let elem in dataObj) {
-                name = `${name}.${elem}`;
-                if (
-                  typeof dataObj[elem] == "object" &&
-                  !Array.isArray(dataObj[elem])
-                ) {
-                  parseObject(dataObj[elem], name);
-                } else if (!Array.isArray(data[elem]) || elem == "range") {
-                  if (elem == "pointerDirection") {
-                    list.push(
-                      <div className="input-field">
-                        <div>{capitalCase(elem)}:</div>
-                        <Field as="select" name={name} key={elem}>
-                          <option value="top">Top</option>
-                          <option value="bottom">Bottom</option>
-                          <option value="left">Left</option>
-                          <option value="right">Right</option>
-                        </Field>
-                      </div>
-                    );
-                  } else if (elem == "path") {
-                    let options = this.state.riverList.map((path) => {
-                      path = path.split(".")[0];
-                      return <option value={path}>{path}</option>;
-                    });
-                    list.push(
-                      <div className="input-field">
-                        <div>{capitalCase(elem)}:</div>
-                        <Field as="select" name={name} key={elem}>
-                          {options}
-                        </Field>
-                      </div>
-                    );
-                  } else {
-                    list.push(
-                      <div className="input-field">
-                        <div>{capitalCase(elem)}:</div>
-                        <Field name={name} key={elem} />
-                      </div>
-                    );
+              let name =
+                feature !== null
+                  ? `[${river}][rapids][${rapid}][${featureName}][${feature}]`
+                  : rapid !== null
+                  ? `[${river}][rapids][${rapid}]`
+                  : river !== null
+                  ? `[${river}]`
+                  : null;
+              let list = [];
+              let tempName = "";
+              const parseObject = (dataObj, name) => {
+                for (let elem in dataObj) {
+                  tempName = `${name}.${elem}`;
+                  if (
+                    typeof dataObj[elem] == "object" &&
+                    !Array.isArray(dataObj[elem])
+                  ) {
+                    parseObject(dataObj[elem], tempName);
+                  } else if (!Array.isArray(data[elem]) || elem == "range") {
+                    if (elem == "pointerDirection") {
+                      list.push(
+                        <div className="input-field">
+                          <div>{capitalCase(elem)}:</div>
+                          <Field as="select" name={tempName} key={elem}>
+                            <option value="top">Top</option>
+                            <option value="bottom">Bottom</option>
+                            <option value="left">Left</option>
+                            <option value="right">Right</option>
+                          </Field>
+                        </div>
+                      );
+                    } else if (elem == "path") {
+                      let options = this.state.riverList.map((path) => {
+                        path = path.split(".")[0];
+                        return <option value={path}>{path}</option>;
+                      });
+                      list.push(
+                        <div className="input-field">
+                          <div>{capitalCase(elem)}:</div>
+                          <Field as="select" name={tempName} key={elem}>
+                            {options}
+                          </Field>
+                        </div>
+                      );
+                    } else {
+                      list.push(
+                        <div className="input-field">
+                          <div>{capitalCase(elem)}:</div>
+                          <Field name={tempName} key={elem} />
+                          {/*{validate(tempName)}*/}
+                        </div>
+                      );
+                    }
                   }
                 }
+                return list;
+              };
 
-                let temp = name.split(".");
-                temp.pop();
-                name = temp.join(".");
-              }
-              return list;
-            };
-
-            return (
-              <Form>
-                {parseObject(data, name)}
-                <button type="submit" className="member" id="submit">
-                  Submit
-                </button>
-              </Form>
-            );
-          }}
-        </Formik>
-      </div>
+              return (
+                <Form>
+                  {parseObject(data, name)}
+                  <button type="submit" className="member" id="submit">
+                    Submit
+                  </button>
+                </Form>
+              );
+            }}
+          </Formik>
+        </div>
+        )
+      </>
     );
   }
 }
