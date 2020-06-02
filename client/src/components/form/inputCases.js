125bb7e88cfebc73024c8dd98129eb8d2fccb836
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-06-02 18:44:43 -0400

    Tooltips added for increased clarity during development.

--- client/src/components/form/inputCases.js
+++ client/src/components/form/inputCases.js
@@ -1,22 +1,26 @@
 import { Field } from "formik";
 import { capitalCase } from "change-case";
+import { Tooltip } from "./inputLabels";
 import React from "react";
 
-export const casePointerDirection = (elem, name) => {
+export const casePointerDirection = (elem, featureType, name) => {
   return (
     <div className="input-field">
       <div>{capitalCase(elem)}:</div>
-      <Field as="select" name={name} key={elem}>
-        <option value="top">Top</option>
-        <option value="bottom">Bottom</option>
-        <option value="left">Left</option>
-        <option value="right">Right</option>
-      </Field>
+      <div className="field-holder">
+        <Field as="select" name={name} key={elem}>
+          <option value="top">Top</option>
+          <option value="bottom">Bottom</option>
+          <option value="left">Left</option>
+          <option value="right">Right</option>
+        </Field>
+        <Tooltip elem={elem} featureType={featureType} />
+      </div>
     </div>
   );
 };
 
-export const caseMapList = (elem, name, mapsList) => {
+export const caseMapList = (elem, featureType, name, mapsList) => {
   let options = mapsList.map((path) => {
     path = path.split(".")[0];
     return <option value={path}>{path}</option>;
@@ -25,14 +29,17 @@ export const caseMapList = (elem, name, mapsList) => {
   return (
     <div className="input-field">
       <div>{capitalCase(elem)}:</div>
-      <Field as="select" name={name} key={elem}>
-        {options}
-      </Field>
+      <div className="field-holder">
+        <Field as="select" name={name} key={elem}>
+          {options}
+        </Field>
+        <Tooltip elem={elem} featureType={featureType} />
+      </div>
     </div>
   );
 };
 
-export const caseArrowList = (elem, name, allRapids) => {
+export const caseArrowList = (elem, featureType, name, allRapids) => {
   let options = [];
   for (let rapid of allRapids.rapids) {
     options.push(<option value={rapid.id}>{rapid.name}</option>);
@@ -41,21 +48,27 @@ export const caseArrowList = (elem, name, allRapids) => {
   return (
     <div className="input-field">
       <div>Link to:</div>
-      <Field as="select" name={name} key={elem}>
-        {options}
-      </Field>
+      <div className="field-holder">
+        <Field as="select" name={name} key={elem}>
+          {options}
+        </Field>
+        <Tooltip elem={elem} featureType={featureType} />
+      </div>
     </div>
   );
 };
 
-export const caseSymbolList = (elem, name) => {
+export const caseSymbolList = (elem, featureType, name) => {
   return (
     <div className="input-field">
       <div>{capitalCase(elem)}</div>
-      <Field as="select" name={name} key={elem}>
-        <option value="caution">Caution</option>
-        <option value="portage">Portage</option>
-      </Field>
+      <div className="field-holder">
+        <Field as="select" name={name} key={elem}>
+          <option value="caution">Caution</option>
+          <option value="portage">Portage</option>
+        </Field>
+        <Tooltip elem={elem} featureType={featureType} />
+      </div>
     </div>
   );
 };
