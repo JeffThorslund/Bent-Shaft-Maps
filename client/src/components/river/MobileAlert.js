49bb771b67ba6b5627602ab28ad65568a8b292d2
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-06-09 00:47:37 -0400

    live update fix

--- client/src/components/river/MobileAlert.js
+++ client/src/components/river/MobileAlert.js
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 import "./MobileAlert.css";
 
 export class MobileAlert extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orientation: null,
+    };
+  }
+
   render() {
     var isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
       navigator.userAgent
@@ -12,6 +19,12 @@ export class MobileAlert extends Component {
     if (isMobile && window.screen.height > window.screen.width) {
       flipYourPhone = <div className="flip">Flip ya dam phone</div>;
     }
+
+    window.onresize = () => {
+      this.setState({
+        orientation: window.screen.orientation.type,
+      });
+    };
 
     return <>{flipYourPhone}</>;
   }
