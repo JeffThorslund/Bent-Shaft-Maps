67987933c0e8d53169e5c5a04b45e0e006a31811
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-06-09 00:15:30 -0400

    mobile alert test

---
+++ client/src/components/river/MobileAlert.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./MobileAlert.css";
+
+export class MobileAlert extends Component {
+  render() {
+    var isMobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+      navigator.userAgent
+    );
+
+    let flipYourPhone = null;
+
+    if (isMobile && window.screen.height > window.screen.width) {
+      flipYourPhone = <div className="flip">Flip ya dam phone</div>;
+    }
+
+    return <>{flipYourPhone}</>;
+  }
+}
+
+export default MobileAlert;
