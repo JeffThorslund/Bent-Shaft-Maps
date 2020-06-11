a54335a247cd4e8704d7b3943601f4516b10f78b
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-06-10 22:27:40 -0400

    force update on all clients when user submits data through form.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import GlobalRouter from "./components/interface/GlobalRouter";
+import io from "socket.io-client";
+const axios = require("axios").default;
 
 const App = () => {
+
+  const [rivers, setRivers] = useState(null);
+
+  const getRiverData = (address, path) => {
+    axios
+      .post(address, {
+        path: path,
+      })
+      .then((response) => {
+        let filtered = response.data.rivers.filter(
+          (river) => river.name !== "Template River"
+        );
+        setRivers(filtered);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    const socket = io();
+    socket.on("connection", () => {
+      console.log("printed from useeffect");
+    });
+    socket.on("update", () => {
+      console.log("Updated from Client")
+      getRiverData("/api/data", "./client/src/river-data");
+    });
+  },[]);
+
   return (
     <Router>
-      <GlobalRouter />
+      <GlobalRouter getRiverData={getRiverData} rivers={rivers}/>
     </Router>
   );
 };
