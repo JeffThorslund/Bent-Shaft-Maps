fbae960a75c568eb05f85ccbcc87d8d57860fac5
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-07-06 23:21:52 -0400

    added mongo integration

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /*--Modules--*/
 const compression = require("compression");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 
@@ -19,7 +20,16 @@ app.use(bodyParser.json({ limit: "50mb" }));
 
 /*--Use Routes--*/
 app.use("/api", handlers);
-app.use("/api", actions)
+app.use("/api", actions);
+
+/*--Connect to Mongo--*/
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 /*--Socket--*/
 var io = require("socket.io")(http);
