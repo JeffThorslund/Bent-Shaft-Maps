42db9c9f60d1169558c87c04dbef03e3d8abc7df
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-07-16 15:53:16 -0400

    fixing varibale names, and doing revisions

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import GlobalRouter from "./components/interface/GlobalRouter";
-import io from "socket.io-client";
-const axios = require("axios").default;
+
+import GlobalRouter from "./components/interface/home/GlobalRouter";
 
 const App = () => {
   const [rivers, setRivers] = useState(null);
 
-  const getRiverData = () => {
+  //fetches an array of rivers from the database
+  useEffect(() => {
     fetch("/api/getData")
       .then((response) => response.json())
       .then((data) => {
@@ -15,22 +15,11 @@ const App = () => {
         setRivers(filtered);
       })
       .catch((err) => console.log(err));
-    }
-
-  // useEffect(() => {
-  //   const socket = io();
-  //   socket.on("connection", () => {
-  //     console.log("printed from useeffect");
-  //   });
-  //   socket.on("update", () => {
-  //     console.log("Updated from Client")
-  //     getRiverData();
-  //   });
-  // }, []);
+  }, [])
 
   return (
     <Router>
-      <GlobalRouter getRiverData={getRiverData} rivers={rivers} />
+      <GlobalRouter rivers={rivers} />
     </Router>
   );
 };
