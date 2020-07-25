04c8dbf968a1c47c8ab3fc80da2e067ab08d59ac
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-07-24 22:04:14 -0400

    add login button that redirects to user interface

---
+++ client/src/components/GenericButton.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../stylesheets/GenericButton.css"
+
+function GenericButton({ text }) {
+  return <div className="GenericButton">{text}</div>;
+}
+
+export default GenericButton;
