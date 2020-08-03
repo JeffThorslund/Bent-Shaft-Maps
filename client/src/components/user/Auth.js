b06ff90d43618334db1d6279fa16b105b35314fc
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-08-03 17:29:41 -0400

    Lots of bits and pieces....unfinished.

--- client/src/components/user/Auth.js
+++ client/src/components/user/Auth.js
@@ -1,21 +1,80 @@
 import React, { useState } from "react";
-import FacebookLogin from "react-facebook-login";
+import { useHistory } from "react-router-dom";
 
-const Auth = (props) => {
+const LoginForm = ({ handleLogin, handleRegister }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const testCaseCreds = [
+    {
+      type: "Valid Login",
+      email: "Test@tester.com",
+      password: "123456",
+    },
+    {
+      type: "Invalid Email",
+      email: "Testtester.com",
+      password: "123456",
+    },
+    {
+      type: "Invalid Password",
+      email: "Test@tester.com",
+      password: "12345",
+    },
+    {
+      type: "Email Does Not Exist",
+      email: "Soccer@tester.com",
+      password: "123456",
+    },
+    {
+      type: "Password Does Not Exist",
+      email: "Test@tester.com",
+      password: "1234567",
+    },
+  ].map((cred) => {
+    return (
+      <div key={cred.type}>
+        <h3>{cred.type}</h3>
+        <div>email: {cred.email}</div>
+        <div>password: {cred.password}</div>
+        <button onClick={(e) => handleLogin(e, cred.email, cred.password)}>
+          Test {cred.type}
+        </button>
+      </div>
+    );
+  });
+
   return (
-    <div>
-      <h1>Choose a Login Method</h1>
-      <FacebookLogin
-        appId="2718766671696939"
-        autoLoad={true}
-        fields="name,email,picture"
-        onClick={() => {
-         return null
-        }}
-        callback={props.responseFacebook}
-      />
-    </div>
+    <>
+      <form onSubmit={(e) => handleLogin(e, email, password)}>
+        <h1>Log In</h1>
+        <label>
+          Email:
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <input type="submit" value="Submit" />
+      </form>
+
+      <input type="button" value="Register" onClick={handleRegister} />
+
+      <h2>Test Cases</h2>
+      {testCaseCreds}
+    </>
   );
 };
 
-export default Auth;
+export default LoginForm;
--- client/src/components/user/UserRouter.js
+++ client/src/components/user/UserRouter.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Auth from "./Auth";
 import UserHome from "./UserHome";
 import { paramCase } from "change-case";
@@ -9,8 +9,12 @@ import {
   Link,
   useParams,
   useRouteMatch,
+  Redirect,
+  useHistory,
+  useLocation,
+  BrowserRouter,
 } from "react-router-dom";
-import { Redirect } from "react-router";
+import axios from "axios";
 
 const UserRouter = (props) => {
   const [credentials, setCredentials] = useState({
@@ -20,67 +24,107 @@ const UserRouter = (props) => {
     isLoggedIn: false,
   });
 
-  const [willRedirect, setWillRedirect] = useState(false);
+  const history = useHistory();
 
-  const responseFacebook = (response) => {
-    //Take authenticated user from Facebook
-    //Send to server
-    //Recieve JWT
+  const register = () => history.push("/register")
+  const login = () => history.push("/login")
 
-    fetch("/api/login", {
-      method: "POST",
-      body: JSON.stringify({
-        name: response.name,
-        email: response.email,
-        userID: response.userID,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.text())
-      .then((data) => {
-        return fetch("/api/authorize", {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${data}`,
-          },
-        });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = (e, email, password) => {
+    e.preventDefault();
+
+    axios
+      .post("/auth/login", {
+        email: email,
+        password: password,
       })
-      .then((response) => response.json())
-      .then((data) => {
-        const { message, authorizedData } = data;
-        console.log(message, authorizedData);
-        if (authorizedData) {
-          setCredentials({
-            name: authorizedData.name,
-            email: authorizedData.email,
-            userID: authorizedData.userID,
-            isLoggedIn: true,
-          });
-          setWillRedirect(true);
-        }
+      .then(function (response) {
+        console.log(response.data);
+        login()
+      })
+      .catch(function (error) {
+        alert(error.response.data);
       });
+
+    console.log(`Submitting Login Data for`, email);
   };
 
   let { path, url } = useRouteMatch();
 
   return (
-    <>
-      {willRedirect && (
-        <Redirect push to={`${url}/${paramCase(credentials.name)}`} />
-      )}
-
+    <BrowserRouter>
       <Switch>
-        <Route exact path={path}>
-          <Auth responseFacebook={responseFacebook} />
+        {/* This will act as authentication home page */}
+        <Route exact path={`${path}/`}>
+          <Auth handleLogin={handleLogin} handleRegister={register}/>
         </Route>
-        <Route path={`${path}/:id`}>
+
+        <Route path={`${path}/register`}>
+          <h1>Registration Page</h1>
+        </Route>
+
+        <PrivateRoute path={`${path}/home`} isLoggedIn={isLoggedIn}>
           <UserHome credentials={credentials} />
-        </Route>
+        </PrivateRoute>
       </Switch>
-    </>
+    </BrowserRouter>
   );
 };
+
+const fakeAuth = {
+  isAuthenticated: true,
+  authenticate(cb) {
+    fakeAuth.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    fakeAuth.isAuthenticated = false;
+    setTimeout(cb, 100);
+  },
+};
+
+// A wrapper for <Route> that redirects to the login
+// screen if you're not yet authenticated.
+function PrivateRoute({ children, ...rest }) {
+  let { path, url } = useRouteMatch();
+  console.log(rest);
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        rest.isLoggedIn ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: `${path}/login`,
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function LoginPage() {
+  let history = useHistory();
+  let location = useLocation();
+
+  let { from } = location.state || { from: { pathname: "/" } };
+  let login = () => {
+    fakeAuth.authenticate(() => {
+      history.replace(from);
+    });
+  };
+
+  return (
+    <div>
+      <p>You must log in to view the page at {from.pathname}</p>
+      <button onClick={login}>Log in</button>
+    </div>
+  );
+}
 
 export default UserRouter;
