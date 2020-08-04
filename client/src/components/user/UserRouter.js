a44f103104cccee8f925b4ceb1a5550972fb00cc
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-08-03 22:22:13 -0400

    Using Context to share User data

--- client/src/components/user/UserRouter.js
+++ client/src/components/user/UserRouter.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import UserContext from "./UserContext";
 import Auth from "./Auth";
 import UserHome from "./UserHome";
 import { paramCase } from "change-case";
@@ -17,17 +18,39 @@ import {
 import axios from "axios";
 
 const UserRouter = (props) => {
-  const [credentials, setCredentials] = useState({
-    name: "",
-    email: "",
-    userID: "",
-    isLoggedIn: false,
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
   });
 
-  const history = useHistory();
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      const token = localStorage.getItem("auth-token");
+      axios
+        .get("/auth/verify", {
+          params: {
+            token: token,
+          },
+        })
+        .then(function (response) {
+          setUserData({
+            token: token,
+            user: { name: response.data.name, email: response.data.email },
+          });
+        })
+        .catch(function (error) {
+          console.log(error.response.data);
+        });
+    };
+    checkLoggedIn();
+  }, []);
 
-  const register = () => history.push("/register")
-  const login = () => history.push("/login")
+  let { path, url } = useRouteMatch();
+
+  //Broken...why?
+  // const history = useHistory();
+  // const register = () => history.push(`user/register`);
+  // const login = () => history.push(`${path}/home`);
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -41,7 +64,8 @@ const UserRouter = (props) => {
       })
       .then(function (response) {
         console.log(response.data);
-        login()
+        localStorage.setItem("auth-token", response.headers["auth-token"]);
+        //login();
       })
       .catch(function (error) {
         alert(error.response.data);
@@ -50,81 +74,26 @@ const UserRouter = (props) => {
     console.log(`Submitting Login Data for`, email);
   };
 
-  let { path, url } = useRouteMatch();
-
   return (
     <BrowserRouter>
-      <Switch>
-        {/* This will act as authentication home page */}
-        <Route exact path={`${path}/`}>
-          <Auth handleLogin={handleLogin} handleRegister={register}/>
-        </Route>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <Switch>
+          {/* This will act as authentication home page */}
+          <Route exact path={`${path}/`}>
+            <Auth handleLogin={handleLogin} /*handleRegister={register}*/ />
+          </Route>
 
-        <Route path={`${path}/register`}>
-          <h1>Registration Page</h1>
-        </Route>
+          <Route path={`${path}/register`}>
+            <h1>Registration Page</h1>
+          </Route>
 
-        <PrivateRoute path={`${path}/home`} isLoggedIn={isLoggedIn}>
-          <UserHome credentials={credentials} />
-        </PrivateRoute>
-      </Switch>
+          <Route path={`${path}/home`} isLoggedIn={isLoggedIn}>
+            <UserHome />
+          </Route>
+        </Switch>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 };
-
-const fakeAuth = {
-  isAuthenticated: true,
-  authenticate(cb) {
-    fakeAuth.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    fakeAuth.isAuthenticated = false;
-    setTimeout(cb, 100);
-  },
-};
-
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
-function PrivateRoute({ children, ...rest }) {
-  let { path, url } = useRouteMatch();
-  console.log(rest);
-  return (
-    <Route
-      {...rest}
-      render={({ location }) =>
-        rest.isLoggedIn ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: `${path}/login`,
-              state: { from: location },
-            }}
-          />
-        )
-      }
-    />
-  );
-}
-
-function LoginPage() {
-  let history = useHistory();
-  let location = useLocation();
-
-  let { from } = location.state || { from: { pathname: "/" } };
-  let login = () => {
-    fakeAuth.authenticate(() => {
-      history.replace(from);
-    });
-  };
-
-  return (
-    <div>
-      <p>You must log in to view the page at {from.pathname}</p>
-      <button onClick={login}>Log in</button>
-    </div>
-  );
-}
 
 export default UserRouter;
