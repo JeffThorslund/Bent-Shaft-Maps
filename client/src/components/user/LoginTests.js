f1497f9e081b91c8f69d808e3751200a5a1f8b8c
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-08-04 00:29:30 -0400

    Styled login page with React Bootstrap

---
+++ client/src/components/user/LoginTests.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+const LoginTests = ({ handleLogin }) => {
+  //Test Cases
+  const testCases = [
+    {
+      type: "Valid Login",
+      email: "Test@tester.com",
+      password: "123456",
+    },
+    {
+      type: "Invalid Email",
+      email: "Testtester.com",
+      password: "123456",
+    },
+    {
+      type: "Invalid Password",
+      email: "Test@tester.com",
+      password: "12345",
+    },
+    {
+      type: "Email Does Not Exist",
+      email: "Soccer@tester.com",
+      password: "123456",
+    },
+    {
+      type: "Password Does Not Exist",
+      email: "Test@tester.com",
+      password: "1234567",
+    },
+  ].map((cred) => {
+    return (
+      <Button
+        onClick={(e) => handleLogin(e, cred.email, cred.password)}
+        key={cred.type}
+        variant="secondary"
+        className="m-1"
+      >
+        Test {cred.type}
+      </Button>
+    );
+  });
+
+  return testCases;
+};
+
+export default LoginTests;
