c3c6ac389db979af476b00329e152e1105a92543
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-09-01 00:26:48 -0400

    smal

--- client/src/components/general/Navigation.js
+++ client/src/components/general/Navigation.js
@@ -3,9 +3,14 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { Link } from "react-router-dom";
 
+/**
+ * Top Navigation Bar
+ * @component
+ */
+
 const Navigation = () => {
   return (
-    <Navbar bg="light" expand="lg" sticky="top">
+    <Navbar bg="light" expand="sm" sticky="top">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
