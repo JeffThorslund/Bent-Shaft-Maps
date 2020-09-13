b7090ff52cde28b58e72f37c423e9dec1f2ea309
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-09-13 13:26:18 -0400

    map label progress

--- client/src/components/river/MapLabel.js
+++ client/src/components/river/MapLabel.js
@@ -3,59 +3,62 @@ import PropTypes from "prop-types";
 import GeneralButton from "../general/GeneralButton";
 import { paramCase } from "change-case";
 
-const MapLabel = (props) => {
-  const { titleTop, titleLeft, pointerDirection } = props.mapLabel;
+const MapLabel = ({
+  url,
+  name,
+  mapLabel: { titleTop, titleLeft, pointerDirection, pointerCoordinates },
+  toggleMap,
+}) => {
 
-  let { pointerCoordinates } = props.mapLabel;
+  // const pointerSetUp = (pointerDirection) => {
+  //   switch (
+  //     pointerDirection // starting coord
+  //   ) {
+  //     case "top":
+  //       return "50,100 ";
 
-  const pointerSetUp = (pointerDirection) => {
-    switch (
-      pointerDirection // starting coord
-    ) {
-      case "top":
-        return "50,100 ";
+  //     case "right":
+  //       return "0,50 ";
 
-      case "right":
-        return "0,50 ";
+  //     case "bottom":
+  //       return "50,0 ";
 
-      case "bottom":
-        return "50,0 ";
+  //     case "left":
+  //       return "100,50 ";
 
-      case "left":
-        return "100,50 ";
+  //     default:
+  //       return "nothing";
+  //   }
+  // };
 
-      default:
-        return "nothing";
-    }
-  };
-
-  pointerCoordinates = pointerCoordinates.join(",");
+  // pointerCoordinates = pointerCoordinates.join(",");
 
   const style = {
-    top: `${titleTop}%`,
+    bottom: `${titleTop}%`,
     left: `${titleLeft}%`,
   };
 
-  const className = `map-label d-flex ${props.mapLabel.pointerDirection}`;
+  const className = `map-label d-flex ${pointerDirection}`;
 
   return (
     <div className={className} style={style}>
       <GeneralButton
-        to={`${props.url}/${paramCase(props.name)}`}
-        text={props.name}
-        onClick={props.toggleMap}
+        to={`${url}/${paramCase(name)}`}
+        text={name}
+        onClick={toggleMap}
         className="map-label-button"
       />
       <svg
         viewBox="0 0 100 100"
-        height="20vh"
+        height="10vw"
         width="10vw"
         xmlns="http://www.w3.org/2000/svg"
         preserveAspectRatio="none"
+        className="label-svg"
       >
         <polyline
           className="pointer"
-          points={`${pointerSetUp(pointerDirection)} ${pointerCoordinates}`}
+          points={"50,0 30,100"}
         />
       </svg>
     </div>
