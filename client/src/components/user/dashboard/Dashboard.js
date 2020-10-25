ea478c401a26ab002033013d0ae7d36e68838549
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-10-24 23:23:09 -0400

    prepping for forms

--- client/src/components/user/dashboard/Dashboard.js
+++ client/src/components/user/dashboard/Dashboard.js
@@ -2,8 +2,8 @@ import React, { useEffect } from "react";
 import Navigation from "../Navigation";
 import { useDispatch, useSelector } from "react-redux";
 import { initializeInterfaceIndex } from "../../../redux/actions/editingAction";
-import EditBasicInfo from "./EditBasicInfo";
 import ItemListGroup from "./ItemListGroup";
+import EditBasicInfo from "./EditBasicInfo";
 
 const Dashboard = () => {
   const dispatch = useDispatch();
@@ -19,38 +19,13 @@ const Dashboard = () => {
     };
   });
 
-  const createInterfaces = (
-    rivers,
-    { riverIndex, sectionIndex, rapidIndex }
-  ) => {
-    const arr = [];
-
-    arr.push(<EditBasicInfo list={rivers} indexType="riverIndex" />);
-
-    if (typeof rivers[riverIndex] !== "undefined") {
-      arr.push(
-        <EditBasicInfo
-          list={rivers[riverIndex].sections}
-          indexType="sectionIndex"
-        />
-      );
-      if (typeof rivers[riverIndex].sections[sectionIndex] !== "undefined") {
-        arr.push(
-          <EditBasicInfo
-            list={rivers[riverIndex].sections[sectionIndex].rapids}
-            indexType="riverIndex"
-          />
-        );
-      }
-    }
-
-    return arr[arr.length - 1];
-  };
-
   return (
     <>
       <Navigation />
-      <ItemListGroup rivers={rivers} editingReducer={editingReducer} />
+      <div className="d-flex flex-row">
+        <ItemListGroup rivers={rivers} editingReducer={editingReducer} />
+        <EditBasicInfo rivers={rivers} editingReducer={editingReducer} />
+      </div>
     </>
   );
 };
--- client/src/components/user/dashboard/EditBasicInfo.js
+++ client/src/components/user/dashboard/EditBasicInfo.js
@@ -1,24 +1,7 @@
 import React from "react";
-import ItemList from "./ItemList";
-// import FormList from "./FormList"
-import { useSelector } from "react-redux";
 
-const EditBasicInfo = ({ list = [], indexType }) => {
-
-  let { rivers, editingReducer } = useSelector((state) => {
-    return {
-      rivers: state.startupReducer.rivers,
-      editingReducer: state.editingReducer,
-    };
-  });
-
-
-  return (
-    <>
-      <ItemList list={list} indexType={indexType} />
-      {/* <FormList list={list}/> */}
-    </>
-  );
+const EditBasicInfo = ({ rivers, editingReducer }) => {
+  return <div>Hello</div>;
 };
 
 export default EditBasicInfo;
