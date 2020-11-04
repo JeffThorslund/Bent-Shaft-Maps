d65fd37749bb3f7c184d9c099cb2e0c02eb11383
Jeff Thorslund <jeffrey.thorslund@gmail.com>
2020-11-04 11:53:36 -0500

    delete redux

--- client/src/redux/reducers/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import startupReducer from "./startupReducer";
-import editingReducer from "./editingReducer";
-
-export default combineReducers({
-  startupReducer, editingReducer
-});
