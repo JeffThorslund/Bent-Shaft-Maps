5437b563256a27fe7510ff13c0672313e4edd5c0
Jeffrey Thorslund <jeffrey.thorslund@gmail.com>
2020-11-24 23:25:14 -0800

    Drag hook!

--- client/src/components/shared/DraggableNode/_utils/useDrag.js
+++ client/src/components/shared/DraggableNode/_utils/useDrag.js
@@ -1,10 +1,57 @@
 import React, { useState, useEffect, useCallback, useMemo } from "react";
 
+//Takes an original position of node
+
 const useDrag = (position) => {
   const [state, setState] = useState({
     isDragging: false,
-    position: POSITION
+    position,
   });
+
+  const handleMouseDown = useCallback(({ clientX, clientY }) => {
+    console.log({ x: clientX, y: clientY });
+
+    setState((state) => ({
+      ...state,
+      isDragging: true,
+      position: { x: clientX, y: clientY },
+    }));
+  }, []);
+
+  const handleMouseMove = useCallback(({ clientX, clientY }) => {
+    const position = {
+      x: clientX,
+      y: clientY,
+    };
+
+    setState((state) => ({
+      ...state,
+      position,
+    }));
+  }, []);
+
+  const handleMouseUp = useCallback(() => {
+    setState((state) => ({
+      ...state,
+      isDragging: false,
+    }));
+  }, []);
+
+  useEffect(() => {
+    if (state.isDragging) {
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("mouseup", handleMouseUp);
+    } else {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    }
+  }, [state.isDragging, handleMouseMove]);
+
+  return {
+    x: state.position.x,
+    y: state.position.y,
+    handleMouseDown,
+  };
 };
 
 export default useDrag;
--- client/src/components/shared/DraggableNode/index.js
+++ client/src/components/shared/DraggableNode/index.js
@@ -1,70 +1,14 @@
 import React, { useState, useEffect, useCallback, useMemo } from "react";
 import useDrag from "./_utils/useDrag";
 
-const DraggableNode = ({
-  onDrag = () => {},
-  onDragEnd = () => {},
-  children,
-  id = 1234,
-}) => {
-  const POSITION = { x: 100, y: 100 };
-
-  const [state, setState] = useState({
-    isDragging: false,
-    position: POSITION
-  });
-
-  const handleMouseDown = useCallback(({ clientX, clientY }) => {
-    console.log({ x: clientX, y: clientY });
-
-    setState((state) => ({
-      ...state,
-      isDragging: true,
-      position: { x: clientX, y: clientY },
-    }));
-  }, []);
-
-  const handleMouseMove = useCallback(
-    ({ clientX, clientY }) => {
-      const position = {
-        x: clientX,
-        y: clientY,
-      };
-
-      setState((state) => ({
-        ...state,
-        position,
-      }));
-    },
-    []
-  );
-
-  const handleMouseUp = useCallback(() => {
-    setState((state) => ({
-      ...state,
-      isDragging: false,
-    }));
-
-    onDragEnd();
-  }, [onDragEnd]);
-
-  useEffect(() => {
-    if (state.isDragging) {
-      window.addEventListener("mousemove", handleMouseMove);
-      window.addEventListener("mouseup", handleMouseUp);
-    } else {
-      window.removeEventListener("mousemove", handleMouseMove);
-      window.removeEventListener("mouseup", handleMouseUp);
-    }
-  }, [state.isDragging, handleMouseMove]);
-
-
-  console.log(useDrag())
+const DraggableNode = () => {
+  
+  const { x, y, handleMouseDown } = useDrag({ x: 100, y: 100 });
 
   return (
     <circle
-      cx={state.position.x}
-      cy={state.position.y}
+      cx={x}
+      cy={y}
       r="50"
       stroke="black"
       stroke-width="3"
