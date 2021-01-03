f98776d7a21487953485d886b8736818344ab357
Jeffrey Thorslund <jeffrey.thorslund@gmail.com>
2021-01-03 00:59:47 -0800

    Delete outdated features

--- client/src/components/rapid/RapidHeader.js
+++ client/src/components/rapid/RapidHeader.js
@@ -2,13 +2,11 @@ import React from "react";
 
 /** The title pf the rapid that shows a name and description based on water level. */
 
-const RapidHeader = ({ name, description, level }) => {
-  return (
-    <div className="rapid-header">
-      <div className="rapid-name"> {name} </div>
-      <div className="rapid-desc">{description}</div>
-    </div>
-  );
-};
+const RapidHeader = ({ name, description, level }) => (
+  <div className="rapid-header">
+    <div className="rapid-name"> {name} </div>
+    <div className="rapid-desc">{description}</div>
+  </div>
+);
 
 export default RapidHeader;
