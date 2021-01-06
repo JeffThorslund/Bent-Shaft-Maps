30355db296ab80f71970a420b9fc5777ebedd037
Jeffrey Thorslund <jeffrey.thorslund@gmail.com>
2021-01-05 20:45:16 -0800

    Unique style classes.

--- client/src/components/shared/Cubic.js
+++ client/src/components/shared/Cubic.js
@@ -4,21 +4,21 @@ const Cubic = ({ typeIndex, lineIndex, pointIndex, reducers, ...props }) => {
   return (
     <g className="ad-Anchor">
       <line
-        className="ad-Anchor-line"
+        className="anchor-line"
         x1={props.p1x}
         y1={props.p1y}
         x2={props.x1}
         y2={props.y1}
       />
       <line
-        className="ad-Anchor-line"
+        className="anchor-line"
         x1={props.p2x}
         y1={props.p2y}
         x2={props.x2}
         y2={props.y2}
       />
       <circle
-        className="ad-Anchor-point"
+        className="bezier-anchor"
         onMouseDown={(e) =>
           reducers.setDraggedCubic({
             lineIndex,
@@ -32,7 +32,7 @@ const Cubic = ({ typeIndex, lineIndex, pointIndex, reducers, ...props }) => {
         r={1}
       />
       <circle
-        className="ad-Anchor-point"
+        className="bezier-anchor"
         onMouseDown={(e) =>
           reducers.setDraggedCubic({
             lineIndex,
