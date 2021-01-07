857510a67041c5db3d9f9ddfd6caf549bcdb063d
Jeffrey Thorslund <jeffrey.thorslund@gmail.com>
2021-01-06 23:53:05 -0800

    Rely on Features.js for svg element creation.

--- client/src/components/shared/Features.js
+++ client/src/components/shared/Features.js
@@ -1,20 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 import SVG from "./SVG";
+import Path from "./Path";
 
 /**
- * Holds all interact-able elements of a rapid
+ * Holds all interact-able elements of a rapid. It should make an array of SVG elements to pass down.
  */
 
-const Features = ({ rapid, reducers, areHandlesVisible, areLinesVisible, areEddysVisible }) => {
+const Features = ({
+  rapid,
+  reducers,
+  areHandlesVisible,
+}) => {
   return (
-    <SVG
-      lines={areLinesVisible.value ? rapid.lines : []}
-      eddys={areEddysVisible.value ? rapid.eddys: []}
-      areHandlesVisible={areHandlesVisible}
-      reducers={reducers}
-      
-    />
+    <SVG reducers={reducers}>
+      {[
+        { features: rapid.lines, featureType: "line" },
+        { features: rapid.eddys, featureType: "eddy" },
+        //{ features: rapid.hydraulics, featureType: "hydraulic" },
+      ].map(({ features, featureType }, typeIndex) =>
+        features.map((feature, i) => (
+          <Path
+            line={feature.vector}
+            featureType={featureType}
+            typeIndex={typeIndex}
+            lineIndex={i}
+            reducers={reducers}
+            areHandlesVisible={areHandlesVisible}
+            key={feature.id}
+          />
+        ))
+      )}
+    </SVG>
   );
 };
 
--- client/src/components/shared/SVG.js
+++ client/src/components/shared/SVG.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useMousePosition, useKeyPress } from "../shared/_utils";
-import Path from "./Path";
 
-const SVG = ({ lines, eddys, reducers, areHandlesVisible }) => {
+const SVG = ({
+  reducers,
+  children,
+}) => {
   const { draggedPoint, draggedCubic } = useSelector(
     (state) => state.testEnvironment
   );
@@ -20,45 +22,19 @@ const SVG = ({ lines, eddys, reducers, areHandlesVisible }) => {
   };
 
   return (
-    
-      <svg
-        className="svg-wrapper"
-        id="vector-container"
-        viewBox="0 0 100 100"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="none"
-        onMouseMove={() => handleMouseMove()}
-        onMouseUp={() => reducers.cancelDragging()}
-        onMouseDown={(e) => isCtrlPressed && reducers.addPoint({ coords })}
-        tabIndex="0"
-      >
-        <g>
-          {lines.map((line, i) => (
-            <Path
-              line={line.vector}
-              typeIndex={0}
-              lineIndex={i}
-              reducers={reducers}
-              areHandlesVisible={areHandlesVisible}
-              key={line.id}
-            />
-          ))}
-        </g>
-        <g>
-          {eddys.map((eddy, i) => (
-            <Path
-              line={eddy.vector}
-              typeIndex={1}
-              lineIndex={i}
-              reducers={reducers}
-              areHandlesVisible={areHandlesVisible}
-              key={eddy.id}
-            />
-          ))}
-        </g>
-      </svg>
-    
-    
+    <svg
+      className="svg-wrapper"
+      id="vector-container"
+      viewBox="0 0 100 100"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="none"
+      onMouseMove={() => handleMouseMove()}
+      onMouseUp={() => reducers.cancelDragging()}
+      onMouseDown={() => isCtrlPressed && reducers.addPoint({ coords })}
+      tabIndex="0"
+    >
+      {children}
+    </svg>
   );
 };
 
