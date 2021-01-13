580b7743a453eff3d0d96d18e7a5913c2279463e
bekatru <teamusicband@gmail.com>
2021-01-13 15:32:14 +0300

    draging works, but with an offset

--- client/src/components/shared/Line.js
+++ client/src/components/shared/Line.js
@@ -8,61 +8,70 @@ import Cubic from "./Cubic";
  */
 
 const Line = ({
-  line,
-  featureType = "line",
-  lineIndex,
-  reducers,
-  areHandlesVisible,
+	position,
+	line,
+	featureType = "line",
+	lineIndex,
+	reducers,
+	areHandlesVisible,
 }) => {
-  return (
-    <>
-      <path
-        onMouseOver={() => reducers.setActiveType({ featureType, lineIndex })}
-        className={featureType}
-        d={buildPath({
-          points: line,
-          closePath: false,
-        })}
-      />
-      {areHandlesVisible.value &&
-        line.map((p, i, a) => {
-          let anchors = [];
-          if (p.c) {
-            anchors.push(
-              <Cubic
-                featureType={featureType}
-                lineIndex={lineIndex}
-                pointIndex={i}
-                reducers={reducers}
-                p1x={a[i - 1].x}
-                p1y={a[i - 1].y}
-                p2x={p.x}
-                p2y={p.y}
-                x1={p.c[0].x}
-                y1={p.c[0].y}
-                x2={p.c[1].x}
-                y2={p.c[1].y}
-                key={i}
-              />
-            );
-          }
-          return (
-            <React.Fragment key={i}>
-              <Point
-                featureType={featureType}
-                lineIndex={lineIndex}
-                pointIndex={i}
-                reducers={reducers}
-                x={p.x}
-                y={p.y}
-                key={i}
-              />
-              {anchors}
-            </React.Fragment>
-          );
-        })}
-    </>
-  );
+	return (
+		<svg
+			className="draggable"
+			x={position.x}
+			y={position.y}
+			onMouseDown={(e) => {
+				reducers.setDraggedFeature();
+				e.stopPropagation();
+			}}
+		>
+			<path
+				onMouseOver={() => reducers.setActiveType({ featureType, lineIndex })}
+				className={featureType}
+				d={buildPath({
+					points: line,
+					closePath: false,
+				})}
+			/>
+			{areHandlesVisible.value &&
+				line.map((p, i, a) => {
+					let anchors = [];
+					if (p.c) {
+						anchors.push(
+							<Cubic
+								featureType={featureType}
+								lineIndex={lineIndex}
+								pointIndex={i}
+								reducers={reducers}
+								p1x={a[i - 1].x}
+								p1y={a[i - 1].y}
+								p2x={p.x}
+								p2y={p.y}
+								x1={p.c[0].x}
+								y1={p.c[0].y}
+								x2={p.c[1].x}
+								y2={p.c[1].y}
+								key={i}
+							/>
+						);
+					}
+					return (
+						<React.Fragment key={i}>
+							<Point
+								featureType={featureType}
+								lineIndex={lineIndex}
+								pointIndex={i}
+								reducers={reducers}
+								x={p.x}
+								y={p.y}
+								key={i}
+							/>
+							{anchors}
+						</React.Fragment>
+					);
+				})}
+		</svg>
+	);
 };
 
 export default Line;
