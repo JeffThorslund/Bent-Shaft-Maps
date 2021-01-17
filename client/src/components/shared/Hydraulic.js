e254ffe819efdd85b40a316a2553e9db12f554f6
Jeffrey Thorslund <jeffrey.thorslund@gmail.com>
2021-01-16 23:42:55 -0800

    Add dead handles to hydraulic

--- client/src/components/shared/Hydraulic.js
+++ client/src/components/shared/Hydraulic.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMousePosition } from "./_utils";
 import Point from "./Point";
 
@@ -13,7 +13,36 @@ const Hydraulic = ({
   reducers,
   areHandlesVisible,
 }) => {
+  const [width, setWidth] = useState(5);
   const coords = useMousePosition();
+
+  // Find handle coordinates
+
+  const center = {
+    x: (line[1].x + line[0].x) / 2,
+    y: (line[1].y + line[0].y) / 2,
+  };
+
+  const perpSetup = [
+    {
+      x: -(line[1].y - line[0].y),
+      y: line[1].x - line[0].x,
+    },
+    {
+      x: line[1].y - line[0].y,
+      y: -(line[1].x - line[0].x),
+    },
+  ];
+
+  const normalMagnitude = Math.sqrt(
+    Math.pow(perpSetup[0].x, 2) + Math.pow(perpSetup[1].y, 2)
+  );
+
+  const perp = perpSetup.map((point) => ({
+    x: ((point.x / normalMagnitude) * width) / 2 + center.x,
+    y: ((point.y / normalMagnitude) * width) / 2 + center.y,
+  }));
+
   return (
     <g
       className="draggable"
@@ -26,7 +55,13 @@ const Hydraulic = ({
         onMouseOver={() => reducers.setActiveType({ featureType, lineIndex })}
         className={featureType}
         d={`M ${line[0].x} ${line[0].y} L ${line[1].x} ${line[1].y}`}
+        strokeWidth={width}
+        stroke="red"
       />
+      {perp.map((point) => (
+        <circle cx={point.x} cy={point.y} r="2" />
+      ))}
+
       {areHandlesVisible.value &&
         line.map((p, i) => (
           <Point
