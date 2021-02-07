fb856be8e062781d4acb345d6366904508f4c0c6
Jeffrey Thorslund <jeffrey.thorslund@gmail.com>
2021-02-06 23:48:27 -0800

    front end wall

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Navigation from '../general/Navigation';
-import Header from './Header';
-import RiverCardContainer from './RiverCardContainer';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navigation from "../general/Navigation";
+import Header from "./Header";
+import RiverCardContainer from "./RiverCardContainer";
 
 /**
  * Holds Navigation Bar, Header, and RiverCardContainer
@@ -10,10 +10,13 @@ import RiverCardContainer from './RiverCardContainer';
 
 const Home = ({ rivers }) => (
   <div className="vh-100 home">
-    <Navigation />
+    {/* <Navigation /> */}
     <Container>
       <Header />
-      <RiverCardContainer rivers={rivers} />
+      {/* <RiverCardContainer rivers={rivers} /> */}
+      <div style={{ textAlign: "center", marginTop: "30px", fontSize: 30 }}>
+        Under Construction.
+      </div>
     </Container>
   </div>
 );
